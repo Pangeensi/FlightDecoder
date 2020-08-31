2d316c938496a9877ced3be28a400d42565ef137
宇智 周 <348272686@qq.com>
2020-08-31 16:11:15 +0800

    版本更新

--- FlightDecoder/FlightDecoder.cpp
+++ FlightDecoder/FlightDecoder.cpp
@@ -4,17 +4,10 @@
 #include <iostream>
 #include "Stack.h"
 #include "Vector.h"
+#include "TCP.h"
+#include "HTTP.h"
 int main()
 {
-    int data[] = { 1,2,3,4,5};
-    Stack<int> s(data,0,sizeof(data)/sizeof(data[0]));
-    std::cout << "pop:" << s.pop() << std::endl;
-    std::cout << "pop:" << s.pop() << std::endl;
-    s.push(6); s.push(7);
-    std::cout << "pop:" << s.pop() << std::endl;
-    std::cout << "pop:" << s.pop() << std::endl;
-    s.push(8); s.push(9); s.push(10);
-    while (!s.empty())
-        std::cout << "pop:" << s.pop() << " ";
+    ADSB_TCP();
     return 0;
 }
--- FlightDecoder/HTTP.h
+++ FlightDecoder/HTTP.h
@@ -1,23 +1,22 @@
 #pragma once
-#ifndef _ADSB_TCP_H_
-#define _ADSB_TCP_H_
+#ifndef _ADSB_HTTP_H_
+#define _ADSB_HTTP_H_
 #include "Vector.h"
 #include "Stack.h"
 unsigned char SynCodeArray[16] = { 0x0e,0xdd,0x24,0x2b,0x37,0xed,0xb8,0x27,0xeb,0x9e,0x60,0xc5,0x08,0x00,0x45,0x00 };	//帧同步数据
-class ADSB_TCP
+class ADSB_HTTP
 {
 private:
 	Stack<unsigned char> SynCode = Stack<unsigned char>(SynCodeArray, 0, 16);		//帧同步字节
 	Vector<unsigned char> ProData = Vector<unsigned char>();						//备用的协议数据头
 	Vector<unsigned char> DataFrame = Vector<unsigned char>();						//数据区
-	const unsigned char _ckWord[2] = { 0x05,0xdc };									//TCP帧校验字
 public:
-	ADSB_TCP()									//TCP帧的构造函数
+	ADSB_HTTP()									//HTTP帧的构造函数
 	{
 		while (!SynCode.empty())
 			printf("%x ", SynCode.pop());
 		std::cout << std::endl;
 	}
-	~ADSB_TCP() {}
+	~ADSB_HTTP() {}
 };
 #endif
